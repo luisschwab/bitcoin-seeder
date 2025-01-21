6020960be3cdaabc78d1056a21b1c97d3a437558
Luis Schwab <luisschwab@protonmail.com>
2025-01-21 12:31:05 -0300

    feat(wire): add NODE_UTREEXO service bit (24)

--- protocol.h
+++ protocol.h
@@ -66,6 +66,7 @@ enum
     NODE_COMPACT_FILTERS = (1 << 6),
     NODE_NETWORK_LIMITED = (1 << 10),
     NODE_P2P_V2 = (1 << 11),
+    NODE_UTREEXO = (1 << 24),
 };
 
 class CAddress : public CService
