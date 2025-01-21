b01c23c15055f7f9c521ce64241feec8cb5773f5
Luis Schwab <luisschwab@protonmail.com>
2025-01-21 12:46:40 -0300

    feat(wire): add NODE_UTREEXO and NODE_COMPACT_FILTERS filters to whitelist

--- main.cpp
+++ main.cpp
@@ -205,20 +205,10 @@ public:
       }
     }
     if (filter_whitelist.empty()) {
-        filter_whitelist.insert(NODE_NETWORK); // x1
-        filter_whitelist.insert(NODE_NETWORK | NODE_BLOOM); // x5
-        filter_whitelist.insert(NODE_NETWORK | NODE_WITNESS); // x9
+        filter_whitelist.insert(NODE_NETWORK | NODE_WITNESS);                        // x9
         filter_whitelist.insert(NODE_NETWORK | NODE_WITNESS | NODE_COMPACT_FILTERS); // x49
-        filter_whitelist.insert(NODE_NETWORK | NODE_WITNESS | NODE_P2P_V2); // x809
-        filter_whitelist.insert(NODE_NETWORK | NODE_WITNESS | NODE_P2P_V2 | NODE_COMPACT_FILTERS); //x849
-        filter_whitelist.insert(NODE_NETWORK | NODE_WITNESS | NODE_BLOOM); // xd
-        filter_whitelist.insert(NODE_NETWORK_LIMITED); // x400
-        filter_whitelist.insert(NODE_NETWORK_LIMITED | NODE_BLOOM); // x404
-        filter_whitelist.insert(NODE_NETWORK_LIMITED | NODE_WITNESS); // x408
-        filter_whitelist.insert(NODE_NETWORK_LIMITED | NODE_WITNESS | NODE_COMPACT_FILTERS); // x448
-        filter_whitelist.insert(NODE_NETWORK_LIMITED | NODE_WITNESS | NODE_P2P_V2); // xc08
-        filter_whitelist.insert(NODE_NETWORK_LIMITED | NODE_WITNESS | NODE_P2P_V2 | NODE_COMPACT_FILTERS); // xc48
-        filter_whitelist.insert(NODE_NETWORK_LIMITED | NODE_WITNESS | NODE_BLOOM); // x40c
+        filter_whitelist.insert(NODE_NETWORK | NODE_WITNESS | NODE_UTREEXO);         // x1000009
+        filter_whitelist.insert(NODE_NETWORK | NODE_WITNESS | NODE_COMPACT_FILTERS | NODE_UTREEXO); // x1000049
     }
     if (host != NULL && ns == NULL) showHelp = true;
     if (showHelp) fprintf(stderr, help, argv[0]);
